8e37ffc34cb299e5ff16136574e2e3c352ab551d
Carlos Oliva <olivam@protonmail.com>
2021-02-28 13:57:52 -0800

    Llené los valores básicos de la plantilla

--- ghc/competition.py
+++ ghc/competition.py
@@ -1,5 +1,5 @@
 # Escribir en este archivo todo el codigo especifico para resolver el problema
 # de la competencia.
 
-PROBLEM_NAME = '<problema>'   # llenar con el nombre del problema
-YEAR = '<año>'                # llenar con el año de la competencia
+PROBLEM_NAME = 'Traffic signaling'   # llenar con el nombre del problema
+YEAR = 2021                # llenar con el año de la competencia
