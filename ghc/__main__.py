bb215ecf36e2afdb82cc1c9d15460f52eff05919
Carlos Oliva <olivam@protonmail.com>
2021-02-28 17:59:12 -0800

    Tremine la heuristica de poner alto de disco en cada interseccion

--- ghc/__main__.py
+++ ghc/__main__.py
@@ -5,10 +5,39 @@ import ghc.competition as cmp
 
 
 if __name__ == '__main__':
-  in_filename, out_filename = ghc.get_args(cmp.PROBLEM_NAME, cmp.YEAR)
   in_desc = [
-    # llenar con la descripcion de los datos de entrada
+    {
+      'name': 'general',
+      'columns': [
+        { 'name': 'sim_duration', 'type': int },
+        { 'name': 'num_inter', 'type': int },
+        { 'name': 'num_streets', 'type': int },
+        { 'name': 'num_cars', 'type': int },
+        { 'name': 'bonus_points', 'type': int }
+      ]
+    },
+    {
+      'name': 'streets',
+      'row_count': 'general num_streets',
+      'columns': [
+        { 'name': 'start_inter_id', 'type': int },
+        { 'name': 'end_inter_id', 'type': int },
+        { 'name': 'street_name', 'type': str },
+        { 'name': 'travel_time', 'type': int }
+      ]
+    },
+    {
+      'name': 'cars',
+      'row_count': 'general num_cars',
+      'columns': [
+        { 'name': 'travel_length', 'type': int },
+        { 'name': 'travel_streets', 'type': str, 'repeat': '*' }
+      ]
+    }
   ]
+  in_filename, out_filename = ghc.get_args(cmp.PROBLEM_NAME, cmp.YEAR)
   in_data = ghc.read_input_file(in_filename, in_desc)
-  # procesar los datos de entrada segun el problema de la competencia
-  # escribir los resultados en un archivo almacenado en la ruta out_filename
+  G = cmp.to_networkx_graph(in_data['streets'])
+  intersections = cmp.get_in_edges_for_every_vertex(G)
+  out_data = cmp.create_schedule_with_stop_signs_only(intersections)
+  cmp.write_output_file(out_filename, out_data)
--- ghc/competition.py
+++ ghc/competition.py
@@ -1,5 +1,73 @@
-# Escribir en este archivo todo el codigo especifico para resolver el problema
-# de la competencia.
+import networkx as nx
 
-PROBLEM_NAME = 'Traffic signaling'   # llenar con el nombre del problema
-YEAR = 2021                # llenar con el año de la competencia
+
+PROBLEM_NAME = 'Traffic signaling'
+YEAR = 2021
+
+
+
+
+def to_networkx_graph(streets):
+  G = nx.DiGraph()
+  for street in streets:
+    G.add_edge(street['start_inter_id'], 
+               street['end_inter_id'], 
+               street_name=street['street_name'],
+               travel_time=street['travel_time'])
+  return G
+
+
+
+def get_in_edges_for_every_vertex(G):
+  in_streets = {}
+  for v in G.nodes.keys():
+    in_edges = G.in_edges(v, data=True)
+    if len(in_edges) > 0:
+      in_streets[v] = []
+      for edge in in_edges:
+        in_streets[v].append(edge[2]['street_name'])
+  return in_streets
+
+
+
+def create_schedule_with_stop_signs_only(intersections):
+  schedule = {
+    'num_inter': len(intersections),
+    'intersections': []
+  }
+  for i in intersections.keys():
+    schedule['intersections'].append(_create_inter_schedule(i, 
+                                                            intersections[i]))
+  return schedule
+
+
+
+# Escribe los datos contenidos en data en un archivo de texto en la direccion
+# especificada por filename.
+# filename (str):   ruta completa del archivo que sera creado
+# data (any):       los datos a almacenar en el archivo
+def write_output_file(filename, 
+                      data):
+  file = open(filename, 'wt')
+  file.write('{}\n'.format(data['num_inter']))
+  for inter in data['intersections']:
+    file.write('{}\n{}\n'.format(inter['id'], inter['num_streets']))
+    for street in inter['streets']:
+      file.write('{} {}\n'.format(street['name'], street['time_on_green']))
+  file.close()
+
+
+
+def _create_inter_schedule(idx, 
+                           in_streets):
+  schedule = {
+    'id': idx,
+    'num_streets': len(in_streets),
+    'streets': []
+  }
+  for street_name in in_streets:
+    schedule['streets'].append({ 
+      'name': street_name,
+      'time_on_green': 1
+    })
+  return schedule
